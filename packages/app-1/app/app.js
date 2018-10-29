863bdd5f4a1ad8003c0114414df9652d54eb2932
Bulent Gorkem <bulent.gorkem@natwestmarkets.com>
2018-10-29 11:25:00 +0000

    mono 1

--- packages/app-1/app/app.js
+++ packages/app-1/app/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import foo from 'library-1';
 
 const Hello = props => (
   <div>
     Hello {props.name} <br />
   </div>
 );
+
+foo();
 
 ReactDOM.render(<Hello name="John Doe" />, document.getElementById('root'));
